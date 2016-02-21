fb37fbc9b3fecc66b333c2a9086d209a3ce6a447
Don Blair <donblair@gmail.com>
2016-02-20 22:57:23 -0500

    update

---
+++ mini-blink/flash_blink_boot_combo_mini.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "set the fuse bits ..."
+
+sudo avrdude -b 19200 -c usbtiny -p m328p -v -e -U efuse:w:0x05:m -U hfuse:w:0xD6:m -U lfuse:w:0xFF:m
+
+
+echo "loading firmware ..."
+
+sudo avrdude -b19200 -c usbtiny -p m328p -v -e -U flash:w:./blink_boot_mini.hex -U lock:w:0x0F:m
+
